4a47ee6c390bb17720a7e00c9cc89b82aff98489
tuananhdatxanh <47851265+tuananhdatxanh@users.noreply.github.com>
2019-02-21 19:18:05 +0700

    Add files via upload

--- faceseo-form.js
+++ faceseo-form.js
@@ -140,7 +140,7 @@ jQuery("#myModalthanh").html(formdiv);
 function thanhxet(){
 
 if(submittedform) {
-	window.location="https://hocvienfaceseo.com";
+	window.location="https://datnentonghop.blogspot.com/";
 	
 	 if(document.cookie.indexOf("adf") == -1)
                    {
